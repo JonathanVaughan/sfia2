8517ecafadb8d9ee06f6bae45a63b44c57cab7e1
JonathanVaughan <jonathan.vaughan@live.com>
2020-06-10 09:22:04 +0000

    jenkins stuff

---
+++ scripts/ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install python -y
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+pip install --user ansible
+ansible --version
+ansible-playbook -v -i inventory.cfg playbook.yaml
